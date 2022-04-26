0d7d196f0ef5d8278edcc8e6bcf02b2e624e0176
Nurieva-E <104027715+Nurieva-E@users.noreply.github.com>
2022-04-26 21:20:05 +0600

    еще одна попытка запушить

--- app/src/main/java/ru/nurieva/loftmoney2/MainActivity.java
+++ app/src/main/java/ru/nurieva/loftmoney2/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.nurieva.loftmoney2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, AddItemActivity.class );
+        startActivity(intent);
 
 
         // TODO: - Here we will work with recyclerview
